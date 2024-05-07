8ca65d7638160f005a6c20b24bef6318d6225fc8
Felix Kröber <felixkroeber@web.de>
2024-05-07 11:07:37 +0200

    preview 0.8.0 (streamlining search & adding automated search mechanism)

--- gsemantique/data/search.py
+++ gsemantique/data/search.py
@@ -1,7 +1,13 @@
+import logging
 import numpy as np
 import pandas as pd
 import planetary_computer as pc
+import pystac
+import xarray as xr
 from pystac_client import Client
+from semantique.processor.core import FakeProcessor
+
+logger = logging.getLogger(__name__)
 
 
 class Finder:
@@ -10,9 +16,7 @@ class Finder:
     This can then be forwarded to STACCube.
     """
 
-    def __init__(
-        self, ds_catalog, t_start, t_end, aoi, provider, collection, layer_key
-    ):
+    def __init__(self, ds_catalog, t_start, t_end, aoi):
         """
         Initializes the Finder object with the specified inputs
 
@@ -21,60 +25,72 @@ class Finder:
             t_start (str): Start time of the search
             t_end (str): End time of the search
             aoi (tbd): Area of interest for the search
-            provider (tbd): tbd
-            collection (tbd): tbd
-            layer_key (tbd): tbd
         """
         self.ds_catalog = ds_catalog
-        self.params_in = {
-            "t_start": t_start,
-            "t_end": t_end,
-            "aoi": aoi,
-            "provider": provider,
-            "collection": collection,
-            "layer_key": layer_key,
-        }
+        self.t_start = t_start
+        self.t_end = t_end
+        self.aoi = aoi
         self.params_search = {}
 
-    def retrieve_params(self):
+    def search_auto(self, recipe, mapping, datacube):
+        # fake run to resolve data references
+        fp = FakeProcessor(
+            recipe=recipe,
+            datacube=datacube,
+            mapping=mapping,
+            extent=xr.DataArray(),
+        )
+        _ = fp.optimize().execute()
+        layer_keys = list(set(fp.cache.seq))
+        # log info
+        logger.info("The recipe references the following data layers:")
+        for key in layer_keys:
+            logger.info(key)
+        # run manual search for each data layer
+        item_colls = []
+        for layer_key in layer_keys:
+            self.search_man(layer_key)
+            item_colls.append(list(self.item_coll))
+        # compile results
+        self.item_coll = [x for sl in item_colls for x in sl]
+        self.item_coll = pystac.ItemCollection(self.item_coll)
+
+    def search_man(self, layer_key):
+        self._retrieve_params(layer_key)
+        self._retrieve_metadata(layer_key)
+        self._postprocess_search(layer_key)
+        logger.info(f"Found {len(self.item_coll):d} datasets")
+
+    def _retrieve_params(self, layer_key):
         """
         Retrieves the data search parameters based on the specified inputs
         """
-        # get specific collection & make sure that it is filtered
-        collection = self.ds_catalog.filter(
-            collection=self.params_in["collection"],
-            provider=self.params_in["provider"],
-        )
-        assert len(collection) == 1
-        collection = collection.iloc[0]
+        # get specific collection
+        ds_table = self.ds_catalog.parse_as_table(keys=None)
+        keep = ds_table["layout_keys"].apply(lambda x: layer_key in x)
+        ds_entry = ds_table[keep].iloc[0]
 
         # retrieve data parameters
-        self.params_search["provider"] = collection["provider"]
-        self.params_search["catalog"] = collection["endpoint"]
-        self.params_search["collection"] = collection["collection"]
-        self.params_search["temp"] = collection["temporality"]
-        self.params_search["lkeys"] = collection["layout_keys"]
-        self.params_search["aoi"] = self.params_in["aoi"]
+        self.params_search["provider"] = ds_entry["provider"]
+        self.params_search["catalog"] = ds_entry["endpoint"]
+        self.params_search["collection"] = ds_entry["collection"]
+        self.params_search["temp"] = ds_entry["temporality"]
+        self.params_search["lkeys"] = ds_entry["layout_keys"]
+        self.params_search["aoi"] = self.aoi
 
         # retrieve time range for query
         if self.params_search["temp"]:
-            self.params_search["t_start"] = np.datetime64(self.params_in["t_start"])
-            self.params_search["t_end"] = np.datetime64(self.params_in["t_end"])
+            self.params_search["t_start"] = np.datetime64(self.t_start)
+            self.params_search["t_end"] = np.datetime64(self.t_end)
         else:
             self.params_search["t_start"] = np.datetime64("1970-01-01")
             self.params_search["t_end"] = np.datetime64("today")
 
-        # ensure that search key is in layout file
-        if self.params_in["layer_key"] not in self.params_search["lkeys"]:
-            raise ValueError(
-                f"Layer key {self.params_in['layer_key']} not found in search parameters."
-            )
-
-    def retrieve_metadata(self):
+    def _retrieve_metadata(self, layer_key):
         """
         Performs the data search based on the retrieved params
         """
-        # init catalog
+        # init search client
         if self.params_search["provider"] == "Planet":
             catalog = Client.open(
                 self.params_search["catalog"], modifier=pc.sign_inplace
@@ -91,54 +107,43 @@ class Finder:
             ],
             intersects=self.params_search["aoi"],
         )
-        item_coll = query.item_collection()
+        self.item_coll = query.item_collection()
 
-        # write layout keys to metadata
-        for item in item_coll:
-            props = item.properties
-            props["semantique:key"] = self.params_in["layer_key"]
-            item.properties = props
-
-        # filter search results
-        self.item_coll = self._postprocess_search(item_coll)
-
-        # print response
-        item_dicts = [item.to_dict() for item in self.item_coll]
-        print(f"Found: {len(item_dicts):d} datasets")
-
-    def postprocess_metadata(self):
-        """
-        Postprocesses the metadata before passing it back
-        """
-        # set collection items datetime (if not provided)
-        for item in self.item_coll:
-            if not item.properties["datetime"]:
-                start_time = pd.Timestamp(item.properties["start_datetime"])
-                end_time = pd.Timestamp(item.properties["end_datetime"])
-                mean_time = start_time + (end_time - start_time) / 2
-                item.set_datetime(mean_time)
-
-    def _postprocess_search(self, item_coll):
+    def _postprocess_search(self, layer_key):
         """
         Method allowing to subset/modify the search results of a STAC search,
         only needed if STAC organisation of data is not standard-conform,
         e.g. if bands are not organised as assets but as items
         """
+        # collection-specific postprocessing
         if (self.params_search["provider"] == "ASF") & (
             self.params_search["collection"] == "sentinel-1-global-coherence"
         ):
-            suffix = self.params_in["layer_key"][-1].rsplit("_", 2)[1:]
+            suffix = layer_key[-1].rsplit("_", 2)[1:]
             if len(suffix) == 2:
                 var, pol = suffix
                 item_coll = [
                     x
-                    for x in item_coll
+                    for x in self.item_coll
                     if x.properties["sar:product_type"] == var.upper()
                     and x.properties["sar:polarizations"] == [pol.upper()]
                 ]
             else:
                 var = suffix[0]
                 item_coll = [
-                    x for x in item_coll if x.properties["sar:product_type"] == var
+                    x for x in self.item_coll if x.properties["sar:product_type"] == var
                 ]
-        return item_coll
+            self.item_coll = item_coll
+
+        # collection-indifferent postprocessing
+        for item in self.item_coll:
+            # write layout keys to metadata
+            props = item.properties
+            props["semantique:key"] = layer_key
+            item.properties = props
+            # set collection items datetimes
+            if not item.properties["datetime"]:
+                start_time = pd.Timestamp(item.properties["start_datetime"])
+                end_time = pd.Timestamp(item.properties["end_datetime"])
+                mean_time = start_time + (end_time - start_time) / 2
+                item.set_datetime(mean_time)
