273ef33d49dbadc14cd9036d8336deac2a6e177a
Felix Kröber <felixkroeber@web.de>
2024-05-15 01:15:05 +0200

    preview 0.17.0 (src: updated dependencies with modified stac-asset)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ dependencies = [
     "multiprocess",
     "planetary-computer @ git+https://github.com/fkroeber/planetary-computer-sdk-for-python.git",
     "semantique @ git+https://github.com/fkroeber/semantique.git@merged_II",
-    "stac-asset",
+    "stac-asset @ git+https://github.com/fkroeber/stac-asset.git@dev",
     "stackstac @ git+https://github.com/fkroeber/stackstac.git",
     "tqdm",
 ]
